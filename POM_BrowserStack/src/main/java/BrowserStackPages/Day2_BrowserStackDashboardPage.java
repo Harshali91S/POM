14d01cc977e54510d2cf00c06651be03be9824cf
yog <yog@gmail.com>
2023-01-17 13:39:07 +0530

    2 modified pagefactory file commit

--- POM_BrowserStack/src/main/java/BrowserStackPages/Day2_BrowserStackDashboardPage.java
+++ POM_BrowserStack/src/main/java/BrowserStackPages/Day2_BrowserStackDashboardPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//Factory code has been added
 public class Day2_BrowserStackDashboardPage 
  {
    // Object Repository
